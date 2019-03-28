19cb7d05df91df18ba9d81b19b90306c111f0f87
egs125 <egs125@gmail.com>
2019-03-28 14:57:25 +0900

    first commit

---
+++ src/Wallet.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Wallet.css';
+
+class Wallet extends Component {
+    render () { 
+        return (
+            <h1>hello</h1>        
+        );
+    }
+}
+
+export default Wallet;
